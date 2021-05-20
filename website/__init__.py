b81bb7dcb8314f63bb9cc1e2eed28d400823070d
Alex Myers <cacloni@hotmail.com>
2021-05-20 15:08:18 +1000

    uncommented user loader

--- website/__init__.py
+++ website/__init__.py
@@ -30,10 +30,10 @@ def create_app():
     login_manager.init_app(app)
 
     #create a user loader function takes userid and returns User
-    #from .models import User  # importing here to avoid circular references
-    #@login_manager.user_loader
-    #def load_user(user_id):
-    #    return User.query.get(int(user_id))
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     #importing views module here to avoid circular references
     # a commonly used practice.
