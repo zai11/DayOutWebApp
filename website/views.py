c4e979ddd9f69acf35b12f36ea1c13ba555bf00b
Neil Woodwards <neilwoodwards@gmail.com>
2021-05-31 14:13:59 +1000

    Add for loop event in events

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
+from .models import Event
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    events=Event.query.all()
+    return render_template('index.html', events=events)
 
 @bp.route('/event')
 def show():
